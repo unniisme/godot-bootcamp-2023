122f0e1e6ac33be4edd7a61515b295d1195188e6
unniisme <122001019@smail.iitpkd.ac.in>
2023-03-08 21:47:10 +0530

    Mid session for my man Nakul

--- Demogorgon/Player.gd
+++ Demogorgon/Player.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
 @export var g = 1000
+@export var speed = 500
+@export var jumpSpeed = 800
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	velocity.x = speed*(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
 	
 	velocity += delta*g*Vector2.DOWN
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y -= jumpSpeed
 
 	move_and_slide()
